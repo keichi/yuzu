c67be15d4ae8c3c1ab433cc5433244978fd4aaf1
Keichi Takahashi <keichi.t@me.com>
2020-05-13 10:52:40 +0900

    Move gRPC collector server to a separate file

--- yuzu/__main__.py
+++ yuzu/__main__.py
@@ -1,14 +1,7 @@
 import os
-from concurrent.futures import ThreadPoolExecutor
-from typing import Dict
 
-import grpc
-
-from .application import Application
+from .collector import create_collector, start_collector
 from .monitor import Monitor
-from .pb.collector_pb2 import TelemetryReply, TimerType
-from .pb.collector_pb2_grpc import (YuzuCollectorServicer,
-                                    add_YuzuCollectorServicer_to_server)
 
 APP_DEF = [{
     "name": "sim",
@@ -27,37 +20,6 @@ APP_DEF = [{
     ],
     "cwd": "/work/keichi/yuzu"
 }]
-
-
-class CollectorServicer(YuzuCollectorServicer):
-    def __init__(self, apps: Dict[str, Application]):
-        self.apps = apps
-
-    def ReportTimer(self, request, context):  # noqa: N802
-
-        app_name = request.common.app_name
-
-        if app_name not in self.apps:
-            print(f"Received timer telemetry from unknown app {app_name}")
-            return TelemetryReply()
-
-        app = self.apps[app_name]
-
-        if request.timer_type == TimerType.READ_IO:
-            app.time_read[request.common.step] = request.duration
-        elif request.timer_type == TimerType.COMPUTE:
-            app.time_compute[request.common.step] = request.duration
-        elif request.timer_type == TimerType.WRITE_IO:
-            app.time_write[request.common.step] = request.duration
-        elif request.timer_type == TimerType.TOTAL:
-            app.time_total[request.common.step] = request.duration
-        else:
-            print(f"Unknown timer type {request.timer_type}")
-
-        return TelemetryReply()
-
-    def ReportDataSize(self, request, context):  # noqa: N802
-        return TelemetryReply()
 
 
 def main() -> None:
@@ -89,14 +51,8 @@ def main() -> None:
     for app_def in APP_DEF:
         monitor.launch(app_def)
 
-    server = grpc.server(ThreadPoolExecutor(max_workers=10))
-    add_YuzuCollectorServicer_to_server(CollectorServicer(monitor.apps),
-                                        server)
-
-    print("Starting gRPC server")
-
-    server.add_insecure_port("[::]:50051")
-    server.start()
+    collector = create_collector(monitor.apps)
+    start_collector(collector)
 
     monitor.wait()
 
---
+++ yuzu/collector.py
@@ -0,0 +1,55 @@
+from concurrent.futures import ThreadPoolExecutor
+from typing import Dict
+
+import grpc
+
+from .application import Application
+from .pb.collector_pb2 import TelemetryReply, TimerType
+from .pb.collector_pb2_grpc import (YuzuCollectorServicer,
+                                    add_YuzuCollectorServicer_to_server)
+
+
+class CollectorServicer(YuzuCollectorServicer):
+    def __init__(self, apps: Dict[str, Application]):
+        self.apps = apps
+
+    def ReportTimer(self, request, context):  # noqa: N802
+
+        app_name = request.common.app_name
+
+        if app_name not in self.apps:
+            print(f"Received timer telemetry from unknown app {app_name}")
+            return TelemetryReply()
+
+        app = self.apps[app_name]
+
+        if request.timer_type == TimerType.READ_IO:
+            app.time_read[request.common.step] = request.duration
+        elif request.timer_type == TimerType.COMPUTE:
+            app.time_compute[request.common.step] = request.duration
+        elif request.timer_type == TimerType.WRITE_IO:
+            app.time_write[request.common.step] = request.duration
+        elif request.timer_type == TimerType.TOTAL:
+            app.time_total[request.common.step] = request.duration
+        else:
+            print(f"Unknown timer type {request.timer_type}")
+
+        return TelemetryReply()
+
+    def ReportDataSize(self, request, context):  # noqa: N802
+        return TelemetryReply()
+
+
+def create_collector(apps):
+    server = grpc.server(ThreadPoolExecutor(max_workers=10))
+    add_YuzuCollectorServicer_to_server(CollectorServicer(apps),
+                                        server)
+
+    return server
+
+
+def start_collector(server):
+    print("Starting gRPC collector server")
+
+    server.add_insecure_port("[::]:50051")
+    server.start()
